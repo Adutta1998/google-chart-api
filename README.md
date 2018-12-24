f0b2baceff3965602a812bbb55af3e3a62a37685
Avinaba Dutta <duttaavinaba98@gmail.com>
2018-12-25 03:19:05 +0530

    Runtimr data implimentation added

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# google-line-chart-with-javascript
+# Runtime Implimentation of Dot Graph
+#### Renewed chart.js file
+
+This is simply create to Impliment runtime data into google chart Api
+
+In some cases we face time problem to load the api.
+To conqure that a simple Javascript pole function is created
+
+```javascript
+/// if google.visualization != undefined then only the drawchart works!!
+var timeout = setInterval(function () {
+        if (google.visualization != undefined) {
+           drawChart();
+           clearInterval(timeout);
+        }
+     }, 300);
+```
+
+## Happy Coding
--- chart.js
+++ chart.js
@@ -1,13 +1,25 @@
-function drawChart(e,sampleData){
-    
+dataArray  = [];
+function genDataArray(x,y){
+    dataArray.push(Array(x,y));
+    console.log(dataArray);
+    //drawChart();  
+    google.charts.load('current', {'packages':['corechart']});
+    var timeout = setInterval(function () {
+        if (google.visualization != undefined) {
+           drawChart();
+           clearInterval(timeout);
+        }
+     }, 300);
+}
+function drawChart(){
     var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Country');
-    data.addColumn('number', 'Population');
-    console.log(sampleData);
-    data.addRows(sampleData);
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Y');
+    data.addRows(dataArray);
+    var chart1 = new google.visualization.LineChart(document.getElementById('chart1'));
+    var chart2 = new google.visualization.ScatterChart(document.getElementById('chart2'));
 
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
-    chart.draw(data,{
-        "title":"Population Chart(countrys with population greater than 100000000)"
-        });
+    chart1.draw(data);
+    chart2.draw(data);
+
 }
